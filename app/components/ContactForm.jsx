8cf336c79a0e924b80cf0cedfbc530463ea87638
Brandon Thomas <lemur767trap@gmail.com>
2024-08-23 15:19:01 -0400

    Resend

--- app/components/ContactForm.jsx
+++ app/components/ContactForm.jsx
@@ -23,7 +23,7 @@ const ContactForm = () => {
 		e.preventDefault();
 		setIsLoading(true);
 		try {
-			const response = await fetch('/api/send', {
+			const response = await fetch('https://www.jc-ww.ca/api/send/', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
